e699f5ed8b99e1a6a10695484caae9089ff34135
Choonghyun Ryu <choonghyun.ryu@gmail.com>
2023-04-12 08:21:53 +0900

    edit skeleton of paged report template

--- inst/figures/hexsticker.R
+++ inst/figures/hexsticker.R
@@ -10,7 +10,7 @@ showtext_auto()
 ## Create hex sticker
 imgurl <- system.file("figures/Report.png", package = "bitReport")
 sticker(imgurl, package = "bitReport", 
-        p_size = 22, p_y = 1.55, p_color = "#31373E", p_family = "teko",
+        p_size = 24, p_y = 1.55, p_color = "#FEB116", p_family = "teko",
         h_fill = "#1E86EE", h_color = "#C76630", 
         s_x = 1, s_y = 0.8, s_width = 0.5, #s_height = .25,
         filename = "man/figures/bitReport_logo.png")
